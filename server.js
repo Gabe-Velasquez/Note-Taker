4df750651470f398107c21c4d0569dac25f011d7
Gabe <gabriel.velasquez0116@gmail.com>
2023-08-27 21:37:53 -0700

    added pathway to start project

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 //importing modules into project
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 //Adding ACs for the project
 // GIVEN a note-taking application
 // WHEN I open the Note Taker
 // THEN I am presented with a landing page with a link to a notes page
-
+app.get('/', (req,res)=>
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
 // WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 
