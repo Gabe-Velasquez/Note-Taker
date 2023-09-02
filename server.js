6dc2d3949a5973ea9a656cffcfb7280d7d682c1a
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-02 15:40:59 -0700

    created function to create note

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-//importing modules into project
+//importing modules/files into project
 const express = require('express');
-const { fstat } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const app = express();
-// const api = require('/public/index.html');
-const PORT = 3008;
+const PORT = process.env.PORT || 3008;
+const entireNotes = require('./db/db.json');
 
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
-//Adding ACs for the project
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
+
 app.get('/', (req,res)=>
     res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
-app.get('/notes', (req,res)=>
+app.get('/notes', (req,res)=>{
     res.sendFile(path.join(__dirname,'public/notes.html'))
-);
+});
 
-app.get('*', (req,res)=>
+app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname,'./public.index.html'))
-)
+});
+
+function createNote(body, notesArray){
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    if (notesArray.length===0)
+        notesArray.push(0);
+    body.id=notesArray[0];
+    notesArray[0]++;
+    notesArray.push(newNote);
+    fs.writeFile(
+        path.join(__dirname,'./db/db.json'),
+        JSON.stringify(notesArray,null,2)
+    );
+    return newNote;
+}
 // WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 
